85f16af1882749b59fa5d6b048823ae37a293254
Afzal <000heyafzal@gmail.com>
2026-06-23 14:50:13 +0530

    Time: 0 ms (100%), Space: 42.1 MB (53.59%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,33 +2,14 @@ class Solution {
     public int climbStairs(int n) {
         if (n <= 2) return n;
 
-        long[][] M = {{1, 1}, {1, 0}};
-        long[][] R = power(M, n);
+        int first = 1, second = 2;
 
-        return (int) R[0][0];
-    }
-
-    private long[][] power(long[][] M, int n) {
-        long[][] res = {{1, 0}, {0, 1}};
-
-        while (n > 0) {
-            if ((n & 1) == 1) res = multiply(res, M);
-            M = multiply(M, M);
-            n >>= 1;
+        for (int i = 3; i <= n; i++) {
+            int current = first + second;
+            first = second;
+            second = current;
         }
-        return res;
-    }
 
-    private long[][] multiply(long[][] A, long[][] B) {
-        return new long[][]{
-            {
-                A[0][0] * B[0][0] + A[0][1] * B[1][0],
-                A[0][0] * B[0][1] + A[0][1] * B[1][1]
-            },
-            {
-                A[1][0] * B[0][0] + A[1][1] * B[1][0],
-                A[1][0] * B[0][1] + A[1][1] * B[1][1]
-            }
-        };
+        return second;
     }
 }
