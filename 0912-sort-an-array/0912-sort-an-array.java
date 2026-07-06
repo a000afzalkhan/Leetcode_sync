20b417a321de3ac6cc365af22f8ff31a9e3cbd8b
Afzal <000heyafzal@gmail.com>
2026-07-06 22:23:41 +0530

    Time: 285 ms (8.24%), Space: 88.2 MB (5.17%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,8 +1,38 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-
-        Arrays.sort(nums);
+        sort(nums, 0, nums.length-1);
         return nums;
-        
+    }
+
+    private void sort(int[] nums, int low, int high){
+        if(low >= high){
+            return ;
+        }
+
+        int s = low;
+        int e = high;
+        int m = s + (e - s)/2;
+        int pivot = nums[m];
+
+        while(s <= e){
+            while(nums[s] < pivot){
+                s++;
+            }
+
+            while(nums[e] > pivot){
+                e--;
+            }
+
+            if(s <= e){
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
+            }
+        }
+
+        sort(nums, low, e);
+        sort(nums,s, high);
     }
 }
