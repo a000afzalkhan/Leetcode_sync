e63d2cf32ba6c53241d2e7aa7c2b5ff518984cd1
Afzal <000heyafzal@gmail.com>
2026-07-29 12:52:37 +0530

    Time: 4 ms (9.21%), Space: 44.3 MB (11.26%) - LeetHub

--- 0065-valid-number/0065-valid-number.java
+++ 0065-valid-number/0065-valid-number.java
@@ -1,42 +1,10 @@
+import java.util.regex.Pattern;
+
 class Solution {
+    private static final Pattern NUMBER_PATTERN = 
+        Pattern.compile("^[+-]?(\\d+\\.?\\d*|\\.\\d+)([eE][+-]?\\d+)?$");
+
     public boolean isNumber(String s) {
-        boolean seenDigit = false;
-        boolean seenDot = false;
-        boolean seenExp = false;
-        boolean digitAfterExp = true;
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            if (Character.isDigit(c)) {
-                seenDigit = true;
-                if (seenExp) {
-                    digitAfterExp = true;
-                }
-            } 
-            else if (c == '.') {
-                if (seenDot || seenExp) {
-                    return false;
-                }
-                seenDot = true;
-            } 
-            else if (c == 'e' || c == 'E') {
-                if (seenExp || !seenDigit) {
-                    return false;
-                }
-                seenExp = true;
-                digitAfterExp = false;
-            } 
-            else if (c == '+' || c == '-') {
-                if (i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {
-                    return false;
-                }
-            } 
-            else {
-                return false;
-            }
-        }
-
-        return seenDigit && digitAfterExp;
+        return NUMBER_PATTERN.matcher(s).matches();
     }
 }
