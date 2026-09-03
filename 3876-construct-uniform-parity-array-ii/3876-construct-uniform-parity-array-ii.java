d91b1c986519d80e6a9702c72c14784010471bd3
Afzal <000heyafzal@gmail.com>
2026-09-03 10:34:34 +0530

    Time: 2 ms (100%), Space: 121.7 MB (62.32%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,40 +1,20 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int n = nums1.length;
-int even = 0;
-int odd = 0;
-
-// Sabse chhote numbers store karne ke liye variables (shuru mein sabse badi value maante hain)
-int minEven = Integer.MAX_VALUE;
-int minOdd = Integer.MAX_VALUE;
-
-// Ek hi loop mein sab kuch count aur find kar lenge
-for (int i = 0; i < n; i++) {
-    int currentNumber = nums1[i];
-    
-    if (currentNumber % 2 == 0) { // Agar Even hai
-        even++;
-        if (currentNumber < minEven) {
-            minEven = currentNumber; // Sabse chhota Even update karo
+        int min=Integer.MAX_VALUE;
+        for(int num:nums1){
+            min=Math.min(min,num);
         }
-    } else {                      // Agar Odd hai
-        odd++;
-        if (currentNumber < minOdd) {
-            minOdd = currentNumber; // Sabse chhota Odd update karo
+
+        //minimum is odd->always possible
+        if(min%2==1){
+            return true;
         }
-    }
-}
-
-// Check 1: Agar poora array pehle se Even ya Odd hai (Aapka logic bilkul sahi tha yahan)
-if (even == n || odd == n) {
-    return true;
-}
-
-// Check 2: Agar array mix hai, toh chhota Odd number, chhote Even se chhota hona chahiye
-if (minOdd < minEven) {
-    return true;
-} else {
-    return false;
-}
+        //minimum is even -> all elements must be even 
+        for(int num:nums1){
+            if(num%2==1){
+                return false;
+            }
+        }
+        return true;
     }
 }
