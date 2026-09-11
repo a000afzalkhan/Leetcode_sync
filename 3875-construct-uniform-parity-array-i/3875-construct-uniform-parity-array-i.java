36069617436e0f270d13c3dd4174fbc506c2df82
Afzal <000heyafzal@gmail.com>
2026-09-11 12:12:06 +0530

    Time: 1 ms (25.59%), Space: 45.7 MB (14.05%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,21 +1,33 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-          int n =nums1.length;
-        int even = 0;
-        int  odd = 0;
+        ArrayList<Integer>nums2 = new ArrayList<>();
 
-        for(int a : nums1){
-            if(a%2==0){
-                even++;
+        // Even 
+        for (int i=0;i<nums1.length;i++){
+            if (nums1[i] % 2 == 0) nums2.add(nums1[i]);
+            else {
+                int j = i ;
+                while (j < nums1.length && nums1[j] % 2 == 0) j++;
+                if (j < nums1.length)
+                nums2.add(nums1[i] - nums1[j]);
             }
-            else{
-                odd++;
-            }
+        }
 
+        if (nums2.size() == nums1.length) return true ;
+
+        // Odd 
+        for (int i=0;i<nums1.length;i++){
+            if (nums1[i] % 2 != 0) nums2.add(nums1[i]);
+            else {
+                int j = i ;
+                while (j < nums1.length && nums1[j] % 2 != 0) j++;
+                if (j < nums1.length)
+                nums2.add(nums1[i] - nums1[j]);
+            }
         }
-        if(even == n || odd ==n){
-            return true;
-        }
-        return true;
+
+        if (nums2.size() == nums1.length) return true ;
+
+        return false ;
     }
 }
