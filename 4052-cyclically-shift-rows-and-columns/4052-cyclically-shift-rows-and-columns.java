52a494e31234a849e0c70c44e82568112d7ac658
Afzal <000heyafzal@gmail.com>
2026-09-22 12:53:14 +0530

    Time: 1 ms (100%), Space: 47.1 MB (94.47%) - LeetHub

---
+++ 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] cyclicShift(int n, int[][] grid, int[] rowShift, int[] colShift) {
+        int ans[][]= new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int newCol= (j-rowShift[i]+n)%n;
+                int newRow= (i-colShift[newCol]+n)%n;
+
+                ans[newRow][newCol]=grid[i][j];
+            }
+        }
+        return ans;
+    }
+}
